5652e47ca9223e20ea8cdec82b6c07a17b0d5636
Juan Minaya <minayaleon@gmail.com>
2021-02-21 18:27:13 -0500

    Added structure for Dialog

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import AlertUI from './src/components/AlertUI';
 import BlockUI from './src/components/BlockUI';
 import DialogUI from './src/components/DialogUI';
+import Dialog2UI from './src/components/Dialog2UI';
 import MainHelper from './src/helpers/MainHelper';
 
-export { AlertUI, BlockUI, DialogUI, MainHelper };
+export { AlertUI, BlockUI, DialogUI, Dialog2UI, MainHelper };
---
+++ src/components/Dialog2UI.js
@@ -0,0 +1,87 @@
+import React, {forwardRef, useImperativeHandle, useState} from 'react';
+import {Button, Paragraph, Dialog, Portal} from 'react-native-paper';
+import {merge, keys, pick, has} from 'lodash';
+
+/**
+ * @typedef Structure
+ * @type {object}
+ * @property {object} nav
+ * @property {object} nav.route
+ * @property {function} nav.route.navigate
+ */
+
+const Dialog2UI = forwardRef((props, ref) => {
+  const txtOk = has(props, 'txtOk') ? props['txtOk'] : 'Ok';
+  const txtCancel = has(props, 'txtCancel') ? props['txtCancel'] : 'Cancel';
+  const btnMode = has(props, 'btnMode') ? props['btnMode'] : 'text';
+
+  const template = {
+    nav: {
+      route: null,
+      screen: '',
+      params: null
+    },
+    btn: {
+      mode: btnMode,
+      btnOk: txtOk,
+      btnCancel: txtCancel,
+    },
+    confirm: false,
+    styles: {
+      title: {},
+      btnOk: {},
+      btnCancel: {marginRight: 10},
+    },
+  };
+
+  const [open, setOpen] = useState(false);
+  const [title, setTitle] = useState('');
+  const [message, setMessage] = useState('');
+  const [structure, setStructure] = useState(template);
+
+  useImperativeHandle(ref, () => ({
+    open: (title, message, structure = null) => {
+      setOpen(true);
+      setTitle(title);
+      setMessage(message);
+
+      if (structure !== null) {
+        const clean = pick(structure, keys(template));
+        const result = merge(template, clean);
+        setStructure(result);
+      }
+    }
+  }));
+
+  return (
+    <Portal>
+      <Dialog
+        visible={open}
+        onDismiss={false}>
+        <Dialog.Title style={structure.styles.title}>{title}</Dialog.Title>
+        <Dialog.Content>
+          <Paragraph>{message}</Paragraph>
+        </Dialog.Content>
+        <Dialog.Actions>
+          {structure.confirm &&
+          <Button style={structure.styles.btnCancel} uppercase={false} mode={structure.btn.mode} onPress={() => {
+            setOpen(false);
+          }}>{structure.btn.cancel}</Button>
+          }
+          <Button style={structure.styles.btnCancel} uppercase={false} mode={structure.btn.mode} onPress={() => {
+            setOpen(false);
+            if (structure.nav.route !== null) {
+              if (structure.nav.params === null) {
+                structure.nav.route.navigate(structure.nav.screen);
+              } else {
+                structure.nav.route.navigate(structure.nav.screen, structure.nav.params);
+              }
+            }
+          }}>{structure.btn.ok}</Button>
+        </Dialog.Actions>
+      </Dialog>
+    </Portal>
+  );
+});
+
+export default Dialog2UI;
